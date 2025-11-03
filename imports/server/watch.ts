0cf14c3adeb2726531f7abbe008742cf5ed0d9d2
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-02 22:56:53 -0700

    Renamed props interface

--- imports/server/watch.ts
+++ imports/server/watch.ts
@@ -6,14 +6,14 @@ export type OnReadyListener = () => void;
 export type OnUnlinkListener = (filePath: string) => void;
 export type OnAddListener = (filePath: string, stats?: Stats) => void;
 
-export interface WatchFilesProps {
+export interface WatchDirProps {
     path: string;
     onReadyListener?: OnReadyListener;
     onUnlinkListener: OnUnlinkListener;
     onAddListener: OnAddListener;
 }
 
-export const watchDir = ({ path, onReadyListener, onUnlinkListener, onAddListener }: WatchFilesProps): FSWatcher => {
+export const watchDir = ({ path, onReadyListener, onUnlinkListener, onAddListener }: WatchDirProps): FSWatcher => {
     const listener = chokidar.watch(path, {
         ignored: (filePath: string, stats?: Stats) => Boolean(stats && stats.isDirectory() && dirname(filePath) === path),
     });
